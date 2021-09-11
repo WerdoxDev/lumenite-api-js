bea455430bfc3c48b68b7ac44013dcd1536f3418
Matin Tat <matin.tat85@gmail.com>
2021-09-11 23:40:30 +0430

    Minor change to npm setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(t|j)sx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
